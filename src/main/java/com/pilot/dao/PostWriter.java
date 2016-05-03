1007c1c4bcf61a8dafcb6cb4a66be850c96e58d4
강승윤 <kangkrkr@estsoft.com>
2016-05-03 14:02:39 +0900

    logback 기능 삽입. 내역을 콘솔 및 파일에 출력하는 기능.

--- src/main/java/com/pilot/dao/PostWriter.java
+++ src/main/java/com/pilot/dao/PostWriter.java
@@ -1,6 +1,8 @@
 package com.pilot.dao;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +15,13 @@ public class PostWriter extends WriterImpl {
 	@Autowired
 	PostService postService;
 	
+	private static final Logger logger = LoggerFactory.getLogger(PostWriter.class);
+	
 	@Override
 	public void write() {
 		Post post = postConstructor();
 		
+		logger.info("사용자가 입력한 게시글의 내용 : {}, 사용자의 이름 : {}", post.getContent(), post.getUser().getName());
 		postService.write(post);
 	}
 }
