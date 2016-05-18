3c9bf9c628d5dd985bb98eb09a33963c03dbcc3c
강승윤 <kangkrkr@estsoft.com>
2016-05-18 13:31:36 +0900

    bower를 사용한 패키지 관리

--- src/main/java/com/pilot/controller/AuthController.java
+++ src/main/java/com/pilot/controller/AuthController.java
@@ -4,9 +4,10 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,18 +25,20 @@ public class AuthController {
 	@Autowired
 	private RedisService redisService;
 	
+	private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 	
 	// 실제 인증을 수행하게 되는 곳.
 	// Authorization Server에서 authorization code를 담아 여기로 redirecting 하게됨.
 	// 여기서 code를 받아 다시 Authorization Server로 부터 code를 기반으로 token을 얻는다.
 	@RequestMapping("/oauth2_callback")
-	public String oauth(@RequestParam("code") String code, @RequestHeader("user-agent") String userAgent,
-												HttpServletRequest request, HttpServletResponse response){
+	public String oauth(@RequestParam("code") String code, HttpServletRequest request, HttpServletResponse response){
 		
 		// key탈취 대비 user-agent , ip 체크 등의 유효성 검사 필요
-		String ipAddress = request.getHeader("X-FORWARDED-FOR");	// PROXY SERVER 또는 LOAD BALANCER를 거쳐나온 클라이언트의 IP
+		// PROXY SERVER 또는 LOAD BALANCER를 거쳐나온 클라이언트의 IP
+		String ipAddress = request.getHeader("X-FORWARDED-FOR");
 		if (ipAddress == null) {
-			ipAddress = request.getRemoteAddr();	// 보편적인 클라이언트 IP를 수집하는 방법
+			// 보편적인 클라이언트 IP를 수집하는 방법
+			ipAddress = request.getRemoteAddr();
 		}
 		
 		Cookie tmIdCookie = CookieUtil.getCookie(request, "tmid");
@@ -47,6 +50,8 @@ public class AuthController {
 		
 		String token = authorizeService.getToken(code);
 		authorizeService.checkUser(tmIdCookie.getValue(), token);
+		
+		logger.info("token : " + token);
 
 		return "redirect:/list?page=1";
 	}
